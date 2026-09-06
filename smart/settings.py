eaf6b28be16b6dc808bcbf1d2dcd5656945cfe6b
Nikhil-Ravula <nikhilravula8374@gmail.com>
2026-09-06 23:57:12 +0530

    refactor: clean STATICFILES_DIRS and strict SECRET_KEY

--- smart/settings.py
+++ smart/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'tracker' / 'static']
+STATICFILES_DIRS = []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_URL = 'login'
